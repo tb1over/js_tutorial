6283667155d0600fe7b4facadd55b3964b6beb82
tb1over <nxsfcc@gmail.com>
2017-12-20 23:45:03 +0800

    add chapt6 chapt7

---
+++ example/dom/chapt6/scripts/showPic.js
@@ -0,0 +1,48 @@
+
+addLoadEvent(prepareGallery);
+
+function showPic(whichpic){
+    if(!document.getElementById('placeholder')) return false;
+    var source = whichpic.getAttribute('href');
+
+    var placeholder = document.getElementById('placeholder');
+    if(placeholder.nodeName != 'IMG') return false;
+    placeholder.setAttribute('src', source);
+
+    //显示描述信息
+    if(document.getElementById('description')){
+        var text =  whichpic.getAttribute('title') ? whichpic.getAttribute('title'): '';
+        var description = document.getElementById('description');
+        if(description.firstChild.nodeType == 3){   //文本节点
+            description.firstChild.nodeValue = text;// p节点里面的第一个文本节点
+        }
+    }
+    return true;
+}
+
+function prepareGallery(){
+    if(!document.getElementsByTagName) return false;
+    if(!document.getElementById) return false;
+    if(!document.getElementById('imagegallery')) return false;
+
+    var gallery = document.getElementById('imagegallery');
+    var links = gallery.getElementsByTagName('a');
+
+    for(let i=0;i<links.length;i++){
+        links[i].onclick = function(){
+            return !showPic(this);
+        }
+    }
+}
+
+function addLoadEvent(func){
+    let oldonload = window.onload;
+    if(typeof window.onload != 'function'){
+        window.onload = func;
+    }else{
+        window.onload = function(){
+            oldonload();
+            func();
+        }
+    }
+}
